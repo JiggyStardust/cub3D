167fe75074eab5c37da9b64777e24700a0c64bbc
JiggyStardust <saaraleni@gmail.com>
2025-03-05 11:14:25 +0200

    libft

---
+++ libft/ft_strndup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 19:23:41 by prynty            #+#    #+#             */
+/*   Updated: 2025/03/05 11:06:40 by sniemela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *src, size_t n)
+{
+	char	*dup;
+	size_t	i;
+
+	i = 0;
+	dup = (char *)malloc(sizeof(*src) * (n + 1));
+	if (!dup)
+		return (NULL);
+	while (src[i] && i < n)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
