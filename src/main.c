4e54d8ed214502d77c4f31e075575a48b84f3bbe
JiggyStardust <saaraleni@gmail.com>
2025-03-14 11:38:34 +0200

    scans and draws the rays pix by pix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 13:29:39 by hpirkola          #+#    #+#             */
-/*   Updated: 2025/03/14 10:06:42 by sniemela         ###   ########.fr       */
+/*   Updated: 2025/03/14 11:36:27 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,15 +42,15 @@ void	cleanup(t_data *data)
 		free(data->map_info.EA);
 }
 
-float	get_ray_length(t_ray *ray, float map_x, float map_y, int i)
+float	get_ray_length(t_ray *ray, float map_x, float map_y, int i, t_data *data)
 {
 	float	hypotenuse;
 	int		x;
 	int		y;
-	float	deg;
 
 	x = (int)map_x;
 	y = (int)map_y;
+	(void)i;
 	// if ((float)map_x < ray->x && fmodf((float)map_x, (int)map_x) != 0)
 	// 	map_x++;
 	// if ((float)map_y < ray->y && fmodf((float)map_y, (int)map_y) != 0)
@@ -58,38 +58,31 @@ float	get_ray_length(t_ray *ray, float map_x, float map_y, int i)
 	ray->dx = fabsf(map_x - ray->x);
 	printf("i: %d\ndx: %f\n", i, ray->dx);
 	ray->dy = fabsf(map_y - ray->y);
-	printf("dy: %f\n\n", ray->dy);
+	printf("dy: %f\n", ray->dy);
 	hypotenuse = sqrt(pow(ray->dx, 2) + pow(ray->dy, 2)); // * cos(PI / 2 - ray.angle); it wasn't supposed to be rayangle, i feel silly
-	// printf("hyp1: %f\n", hypotenuse);
-	deg = fabs(33.0 - i) / 180 * PI;
-	hypotenuse = hypotenuse * cos(deg);
-	printf("hyp2: %f\n", hypotenuse);
+	// printf("\n\nhyp1: %f\n", hypotenuse);
+	hypotenuse = hypotenuse * cos(data->player.angle - ray->angle);
+	// printf("hyp2: %f\n", hypotenuse);
 	// printf("the i: %d, abs division: %f\nthe multiplier: %f\n", i, deg, cos(deg));
 	return (hypotenuse);
 }
 
-mlx_image_t *draw_ray(t_data *data, t_ray ray, int i, mlx_image_t *img)
+mlx_image_t *draw_ray(t_data *data, t_ray ray, int x, mlx_image_t *img)
 {
 
 	float	center;
 	float	wall_height;
 	int		y;
-	int		x;
 	(void)data;
 
 	center = HEIGHT / 2;
-	wall_height = HEIGHT / ray.len; // don't divide by float, slight pixel changes are seen.
-	// printf("raylen = %f\n", ray.len);
-	x = 0;
-	while (x <= WIDTH / 67)
+	wall_height = HEIGHT / ray.len;
+	printf("raylen = %f\n", ray.len);
+	y = center - wall_height / 2;
+	while (y <= center + wall_height / 2)
 	{
-		y = center - wall_height / 2;
-		while (y <= center + wall_height / 2)
-		{
-			mlx_put_pixel(img, WIDTH * i / 67 + x, y, RED);
-			y++;
-		}
-		x++;
+		mlx_put_pixel(img, x, y, RED);
+		y++;
 	}
 	return (img);
 }
@@ -107,50 +100,31 @@ mlx_image_t	*raycaster(t_data *data)
 	ray.x = data->player.x;
 	ray.y = data->player.y;
 	ray.angle = data->player.angle - (33.0 / 180 * PI);
-	if (ray.angle == 2 * PI)
-		ray.angle = 0;
+	if (ray.angle > 2 * PI)
+		ray.angle -= 2 * PI;
 	else if (ray.angle < 0)
 		ray.angle += 2 * PI;
 	i = 0;
-	// while (i < 33)
-	// {
-	// 	map_x = ray.x;
-	// 	map_y = ray.y;
-	// 	xy = get_index_of_rov_and_col(data, (int)(data->player.x), (int)(data->player.y), 1);
-	// 	while (data->map[xy] == FLOOR)
-	// 	{
-	// 		// map_x -= cos(ray.angle);
-	// 		// map_y -= sin(ray.angle);
-	// 		xy = get_index_of_rov_and_col(data, map_x - cos(ray.angle), map_y - sin(ray.angle), 1);
-	// 		if (data->map[xy] == FLOOR)
-
-	// 	}
-	// 	ray.len = get_ray_length(&ray, map_x, map_y, i);
-	// 	draw_ray(data, ray, i, img);
-	// 	ray.angle += (1.0/180 * PI);
-	// 	if (ray.angle == 2 * PI)
-	// 		ray.angle = 0;
-	// 	i++;
-	// }
-	while (i <= 66)
+	while (i < WIDTH)
 	{
 		map_x = ray.x;
 		map_y = ray.y;
 		xy = get_index_of_rov_and_col(data, (int)(data->player.x), (int)(data->player.y), 1);
 		while (data->map[xy] == FLOOR)
 		{
-			xy = get_index_of_rov_and_col(data, map_x - cos(ray.angle) * 0.01, map_y - sin(ray.angle) * 0.01, 1);
+			xy = get_index_of_rov_and_col(data, (int)(map_x - cos(ray.angle) * 0.01), (int)(map_y - sin(ray.angle) * 0.01), 1);
 			if (data->map[xy] == FLOOR)
 			{
-				map_x -= cos(ray.angle) * 0.01;
 				map_y -= sin(ray.angle) * 0.01;
+				map_x -= cos(ray.angle) * 0.01;
 			}
 		}
-		ray.len = get_ray_length(&ray, map_x, map_y, i);
+		ray.len = get_ray_length(&ray, map_x, map_y, i, data);
 		draw_ray(data, ray, i, img);
-		ray.angle += (1.0/180 * PI);
-		if (ray.angle == 2 * PI)
-			ray.angle = 0;
+		ray.angle += ((66.0 / 180 * PI) / WIDTH);
+		if (ray.angle > 2 * PI)
+			ray.angle -= 2 * PI;
+		// printf("ray.angle: %f, player.angle: %f\n\n", ray.angle, data->player.angle);
 		i++;
 	}
 	return (img);
