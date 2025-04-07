a0395acca7a446755345eacb034cd12433282103
JiggyStardust <saaraleni@gmail.com>
2025-04-07 15:31:12 +0300

    if minimap too big, it's not drawn

--- src/images_to_window_bonus.c
+++ src/images_to_window_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 16:22:23 by sniemela          #+#    #+#             */
-/*   Updated: 2025/04/07 14:06:12 by sniemela         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:27:50 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ bool	images_to_window(t_data *data)
 		return (false);
 	if	(mlx_image_to_window(data->mlx, data->view, 0, 0) == -1)
 		return (false);
-	if (mlx_image_to_window(data->mlx, data->minimap, 0, 0) == -1)
-		return (false);
-	if (mlx_image_to_window(data->mlx, data->mini_p_img, data->player.x \
-			* data->tile_mini, data->player.y * data->tile_mini) == -1)
-		return (false);
+	if (data->minimap)
+	{
+		if (mlx_image_to_window(data->mlx, data->minimap, 0, 0) == -1)
+			return (false);
+		if (mlx_image_to_window(data->mlx, data->mini_p_img, data->player.x \
+				* data->tile_mini, data->player.y * data->tile_mini) == -1)
+			return (false);
+	}
 	return (true);
 }
--- src/terminate_n_free_bonus.c
+++ src/terminate_n_free_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:23:21 by sniemela          #+#    #+#             */
-/*   Updated: 2025/03/04 17:23:26 by sniemela         ###   ########.fr       */
+/*   Updated: 2025/04/07 15:08:44 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,14 @@ static void	delete_images(t_data *data)
 {
 	if (data->mini_p_img)
 		mlx_delete_image(data->mlx, data->mini_p_img);
-	if (data->mini_f_img)
-		mlx_delete_image(data->mlx, data->mini_f_img);
-	if (data->mini_w_img)
-		mlx_delete_image(data->mlx, data->mini_w_img);
+	// if (data->mini_f_img)
+	// 	mlx_delete_image(data->mlx, data->mini_f_img);
+	// if (data->mini_w_img)
+	// 	mlx_delete_image(data->mlx, data->mini_w_img);
+	if (data->minimap)
+		mlx_delete_image(data->mlx, data->minimap);
+	if (data->view)
+		mlx_delete_image(data->mlx, data->view);
 }
 
 void	terminate_free(t_data *data, int error, char *message)
